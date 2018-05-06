95038c76b8899a1cdcb51c80b08c597f5a19191a
Jerome <jerome.studer@student.unisg.ch>
2018-05-06 22:38:09 +0200

    V05.06.18

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-io.on('connection' (socket) =>{
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+
+
+io.on('connection' (socket)=>{
+  
   socket.on('disconnect',function(){
     console.log('disconnect');
   });
@@ -11,6 +17,6 @@ io.on('connection' (socket) =>{
   });
 });
 
-http.listen(5000,() =>{
-  console.log('started on port 5000');
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
