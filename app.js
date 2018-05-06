39736ac9fddd03439c5ec69ba6e6395c6157d7ab
Jerome <jerome.studer@student.unisg.ch>
2018-05-06 23:13:54 +0200

    V6

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var server = require('http').createServer(app);
-var io = require('../..')(server);
+var io = require('socket.io')(server);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
