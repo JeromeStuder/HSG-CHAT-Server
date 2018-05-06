66dd2817460a5c6cb1c75a9cd258718c2021c3cb
Jerome <jerome.studer@student.unisg.ch>
2018-05-07 00:06:45 +0200

    V10

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
-server.listen(server_port, server_ip_address, () => {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+server.listen(server_port, () => {
+  console.log( "Listening on " + ", port " + server_port )
 });
 
 app.get('/', function (req, res) {
